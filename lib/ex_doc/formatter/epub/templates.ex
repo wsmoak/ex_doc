2ad0b17ba8009adecafcbd9bd4dc95b31975f78c
Milton Mazzarri <milmazz@gmail.com>
2015-09-16 19:33:46 -0500

    Reuse functions from HTML formatter

--- lib/ex_doc/formatter/epub/templates.ex
+++ lib/ex_doc/formatter/epub/templates.ex
@@ -4,6 +4,7 @@ defmodule ExDoc.Formatter.EPUB.Templates do
   """
 
   require EEx
+  alias ExDoc.Formatter.HTML.Templates, as: HTMLTemplates
 
   @content_template_doc """
   Creates the [Package Document Definition](http://www.idpf.org/epub/30/spec/epub30-publications.html#sec-package-def),
@@ -48,60 +49,8 @@ defmodule ExDoc.Formatter.EPUB.Templates do
   Generate content from the module template for a given `node`
   """
   def module_page(config, node) do
-    types       = node.typespecs
-    functions   = Enum.filter node.docs, & &1.type in [:def]
-    macros      = Enum.filter node.docs, & &1.type in [:defmacro]
-    callbacks   = Enum.filter node.docs, & &1.type in [:defcallback, :defmacrocallback]
-    module_template(config, node, types, functions, macros, callbacks)
-  end
-
-  # Get the full specs from a function, already in HTML form.
-  defp get_specs(%ExDoc.FunctionNode{specs: specs}) when is_list(specs) do
-    presence specs
-  end
-
-  defp get_specs(_node), do: nil
-
-  # Convert markdown to HTML.
-  defp to_html(nil), do: nil
-  defp to_html(bin) when is_binary(bin), do: ExDoc.Markdown.to_html(bin)
-
-  # Get the pretty name of a function node
-  defp pretty_type(%ExDoc.FunctionNode{type: t}) do
-    case t do
-      :def              -> "function"
-      :defmacro         -> "macro"
-      :defcallback      -> "callback"
-      :defmacrocallback -> "macro callback"
-      :type             -> "type"
-    end
-  end
-
-  # Generate a link id
-  defp link_id(node), do: link_id(node.id, node.type)
-  defp link_id(id, type) do
-    case type do
-      :defmacrocallback -> "c:#{id}"
-      :defcallback      -> "c:#{id}"
-      :type             -> "t:#{id}"
-      _                 -> "#{id}"
-    end
-  end
-
-  # Get the first paragraph of the documentation of a node, if any.
-  defp synopsis(nil), do: nil
-  defp synopsis(doc) do
-    String.split(doc, ~r/\n\s*\n/) |> hd |> String.strip() |> String.rstrip(?.)
-  end
-
-  defp presence([]),    do: nil
-  defp presence(other), do: other
-
-  defp h(binary) do
-    escape_map = [{"&", "&amp;"}, {"<", "&lt;"}, {">", "&gt;"}, {"\"", "&quot;"}]
-    Enum.reduce escape_map, binary, fn({pattern, escape}, acc) ->
-      String.replace(acc, pattern, escape)
-    end
+    types = HTMLTemplates.group_types(node)
+    module_template(config, node, types.types, types.functions, types.macros, types.callbacks)
   end
 
   # Keep only valid characters for an XHTML ID element
